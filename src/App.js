456726fc0c7c47be45a0b2841cc2bcd45eba8b08
Christian Johansen <137861@RTS-365.dk>
2021-01-19 10:48:23 +0100

    noget der ikke virker

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import Numberholder from './components/Numberholder';
-import Randomnumber from './components/Randomnumber';
+
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Randomnumber />
+        
        
         <Numberholder />
       </header>
--- src/components/Numberholder.js
+++ src/components/Numberholder.js
@@ -3,11 +3,30 @@ import { css } from '@emotion/react';
 
 
 const Numberholder = () => {
-
+function pickanumber(){
     let myNumb = [1,2,3,4,5,6,7,8,9,10]
-    const mixedResults = myNumb.sort(() => Math.random() - 0.5);
-    let tablenumb = mixedResults[0]
-    const tabelnummer = tablenumb;
+    let mixedResults = myNumb.sort(() => Math.random() - 0.5);
+
+    return mixedResults[0]
+}
+   
+    const tabelnummer = pickanumber()
+    /*const tabelnummer = tablenumb;*/
+    console.log (tabelnummer)
+    
+    let myNumber = [tabelnummer*1,tabelnummer*2,tabelnummer*3,tabelnummer*4,tabelnummer*5,tabelnummer*6,tabelnummer*7,tabelnummer*8,tabelnummer*9,tabelnummer*10]
+    console.log (myNumber)
+    
+    let mixed = myNumber.sort(() => Math.random() - 0.5);
+
+   console.log ("hephephep")
+    
+  
+   console.log (mixed)
+  
+   
+
+   
 
     
 
@@ -24,37 +43,28 @@ const Numberholder = () => {
     margin: 0.5rem;
     border-radius:30%;
     `;
-function test (e) {
-
-    let myNumb4 = [4,8,12,16,20,24,28,32,36,40]
-    const mixed = myNumb4.sort(() => Math.random() - 0.5);
-
-    console.log ("hephephep")
-
-    console.log (tabelnummer)
-    console.log (mixed[0])
-
-    console.log (mixed[0] * tabelnummer)
-
-   
-    
-}
 
 
+console.log (tabelnummer)
 
     return ( 
-
         <div>
-<button css={testbutton} onClick={test}>{tabelnummer} x  1</button>
-<button css={testbutton} onClick={test}>{tabelnummer} x  2</button>
-<button css={testbutton} onClick={test}>{tabelnummer} x  3 </button>
-<button css={testbutton} onClick={test}>{tabelnummer} x  4</button>
-<button css={testbutton} onClick={test}>{tabelnummer} x  5</button>
-<button css={testbutton} onClick={test}>{tabelnummer} x  6</button>
-<button css={testbutton} onClick={test}>{tabelnummer} x  7</button>
-<button css={testbutton} onClick={test}>{tabelnummer} x  8</button>
-<button css={testbutton} onClick={test}>{tabelnummer} x  9</button>
-<button css={testbutton} onClick={test}>{tabelnummer} x 10</button>
+
+        <h1> {tabelnummer}-tabellen </h1> 
+
+        
+
+        
+<button css={testbutton} >{tabelnummer} x  1</button>
+<button css={testbutton} >{tabelnummer} x  2</button>
+<button css={testbutton} >{tabelnummer} x  3 </button>
+<button css={testbutton} >{tabelnummer} x  4</button>
+<button css={testbutton} >{tabelnummer} x  5</button>
+<button css={testbutton} >{tabelnummer} x  6</button>
+<button css={testbutton} >{tabelnummer} x  7</button>
+<button css={testbutton} >{tabelnummer} x  8</button>
+<button css={testbutton} >{tabelnummer} x  9</button>
+<button css={testbutton} >{tabelnummer} x 10</button>
 </div>
 
      );
